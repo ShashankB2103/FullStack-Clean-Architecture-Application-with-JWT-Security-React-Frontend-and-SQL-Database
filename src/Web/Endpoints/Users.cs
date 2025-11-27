4ca04d9698fe1d63bb2430e27943ca59913e5abb
Shashank Basavaraju <shashankb2103@gmail.com>
2025-11-27 13:38:12 +0530

    Add files via upload

---
+++ src/Web/Endpoints/Users.cs
@@ -0,0 +1,13 @@
+﻿#if (UseApiOnly)
+using CleanArchitecture.Infrastructure.Identity;
+
+namespace CleanArchitecture.Web.Endpoints;
+
+public class Users : EndpointGroupBase
+{
+    public override void Map(RouteGroupBuilder groupBuilder)
+    {
+        groupBuilder.MapIdentityApi<ApplicationUser>();
+    }
+}
+#endif
